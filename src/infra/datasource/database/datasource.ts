94505c8e70ab3715d36dd292d84472c0eee51cbe
mmrosatab <mmrosadev@gmail.com>
2024-10-18 18:34:22 -0300

    chore: create database connection by typeorm

---
+++ src/infra/datasource/database/datasource.ts
@@ -0,0 +1,28 @@
+import { DataSource } from "typeorm"
+import { BookEntity } from './entities/BookEntity'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const defaultDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: true,
+  entities: [BookEntity],
+  subscribers: [],
+  migrations: ['./src/**/migrations/*.ts'],
+})
+
+
+export async function initDatabase(): Promise<void> {
+  await defaultDataSource.initialize()
+}
+
+export async function finishDatabase(): Promise<void> {
+  await defaultDataSource.destroy()
+}
