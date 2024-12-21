e8c6e725ebd2439a31504222ad4c880eebe20ef8
mmrosatab <mmrosatab@hotmail.com>
2024-12-21 20:24:03 -0300

    docs: add swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Koa from "koa"
 import Router from "koa-router"
 import bodyParser from "koa-bodyparser"
+import { koaSwagger } from 'koa2-swagger-ui'
+import serveStatic from 'koa-static'
+import path from 'path'
 import { initDatabase } from "./infra/datasource"
 import { routesBook } from './infra/http'
 import { BookCreateService } from "./domain/book/useCase"
@@ -8,11 +11,21 @@ import { BookCreateRepository } from "./infra"
 
 (async function init() {
   await initDatabase()
-  
+
   const PORT = process.env.PORT || 3000
   const app = new Koa()
   const router = new Router()
 
+  app.use(serveStatic(path.join(__dirname, '..', 'docs')))
+
+  app.use(
+    koaSwagger({
+      routePrefix: '/docs',
+      swaggerOptions: {
+        url: 'openapi.yml',
+      },
+    })
+  );
   app.use(bodyParser())
 
   const bookCreaterRepository = new BookCreateRepository()
@@ -26,7 +39,7 @@ import { BookCreateRepository } from "./infra"
   app
     .use(router.routes())
     .use(router.allowedMethods())
-    console.info("Data Source has been initialized!")
+  console.info("Data Source has been initialized!")
 
   app.listen(PORT, () => {
     console.info(`Server running on port ${PORT}`)
