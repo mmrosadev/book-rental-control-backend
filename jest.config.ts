9169809624aa975d8a8f802c2fe8e738b5805b3f
mmrosatab <mmrosatab@hotmail.com>
2025-01-19 20:35:23 -0300

    config: install and config jest

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1"
+    },
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    collectCoverage: false,
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+    coverageDirectory: 'coverage',
+}
+
+export default config
